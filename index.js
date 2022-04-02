2f5cf157744466ce1a0cd676c4a4c81646ac7a10
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-04-02 16:20:41 -0500

    Passing data by reference or by value

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//git init
+//pass by value or pass by reference
+//this questions wants to know how much your work with data
+/**When you are working with primitive data types data is always passed by value. */
+//Passing by value primitive data types
+let ex1 = 1;
+let ex2 = ex1;
+ex2++;
+
+console.log(ex1, " - ", ex2);
+//Reference non-primitive data types
+const example3 = [];
+const example4 = example3;
+example4.push("ref");
+
+console.log(example3, " ---- ", example4);
+/** with non-primitive data types you are passing values by reference. The only way around this is to use the spread operator so that you are actually just making a copy of that value instead of doing a direct reference.  */
